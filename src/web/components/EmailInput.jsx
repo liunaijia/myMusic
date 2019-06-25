466b84d2d7fb93d14288ec67750da0a5089667b7
Naijia Liu <naijialiu@geniesolutions.com.au>
2019-06-25 14:23:43 +1000

    remove redundant object destructing

--- src/web/components/EmailInput.jsx
+++ src/web/components/EmailInput.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Input from './Input';
 
-function EmailInput({ ...props }) {
+function EmailInput(props) {
   return (
     <Input type="email" label="Email" autoComplete="email" {...props} />
   );
--- src/web/components/PasswordInput.jsx
+++ src/web/components/PasswordInput.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Input from './Input';
 
-function PasswordInput({ ...props }) {
+function PasswordInput(props) {
   return (
     <Input type="password" label="Password" autoComplete="current-password" {...props} />
   );
